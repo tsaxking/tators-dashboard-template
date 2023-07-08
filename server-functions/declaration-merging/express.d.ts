2cb60a5e894c1fb272e91b5e38e43e1e47867232
tsaxking <81051058+tsaxking@users.noreply.github.com>
2023-07-07 21:29:43 -0600

    server-caching

--- server-functions/declaration-merging/express.d.ts
+++ server-functions/declaration-merging/express.d.ts
@@ -1,11 +1,36 @@
-// declare global {
-//     namespace Express {
-//         interface Request {
-//             session: import('../structure/sessions').Session;
-//             start: number;
-//             io: import('socket.io').Server;
+// declare module 'express' {
+//     export default function (req: Request, res: Response, next: NextFunction): void;
+
+//     export interface Request {
+//         session: import('../structure/sessions').Session;
+//         ip: string;
+//         url: string;
+//         body: any;
+//         headers: any;
+//         cookies: any;
+//         signedCookies: any;
+//         query: any;
+//         params: any;
+//         method: string;
+//         originalUrl: string;
+//         path: string;
+//         protocol: string;
+
+//         io: import('socket.io').Server;
+//         start: number;
+//         file: {
+//             id: string;
+//             name: string;
+//             size: number;
+//             type: string;
+//             ext: string;
+//             contentType: string;
+//             filename: string
 //         }
 //     }
-// }
 
-// export {}
+//     interface Response {
+//         send(data: any): Response;
+//         sendFile(path: string): Response;
+//     }
+// }
